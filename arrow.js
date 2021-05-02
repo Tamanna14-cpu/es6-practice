25b3bf65544c15badb9a68a51709362cd0764a4a
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2021-05-02 21:56:46 +0600

    arrow function done

---
+++ arrow.js
@@ -0,0 +1,45 @@
+// vanilla way
+// function doubleIt (num){
+//     return num * 2;
+// }
+
+// const result = doubleIt(5);
+// console.log(result);
+
+
+
+// // 2nd way
+// const doubleIt = function(num){
+//     return num * 2;
+// }
+// const result = doubleIt(10);
+// console.log(result);
+
+
+// es6
+// single parameter
+const doubleIt = num => num * 2;
+const result = doubleIt(10);
+console.log(result);
+
+// double parameter
+const add = (x, y) => x + y;
+const result2 = add(4, 7);
+console.log(result2);
+
+// without parameter
+const give5 = () => 5;
+const result3 = give5();
+console.log(result3);
+
+
+// functioner vitor onkgula kajer jonno
+const doMath = (x, y) => {
+    const sum = x + y;
+    const diff = x - y;
+    const result = sum * diff;
+    return result;
+}
+
+const result4 = doMath(7, 3);
+console.log(result4);
