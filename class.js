1b44edb7a16b645ed50048b635c601557e243344
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2021-05-03 10:31:42 +0600

    create object from class

---
+++ class.js
@@ -0,0 +1,14 @@
+class student {
+     constructor(sId, sName){
+         this.id = sId;
+         this.name = sName;
+         this.school = 'kolimunnesa school';
+     }
+}
+
+const student1 = new student(12, 'shuvo');
+const student2 = new student(22, 'mahi');
+console.log(student1, student2);
+
+
+// create object from class. aikhane school hoilo akta shared element jeta sobar jonno common.
